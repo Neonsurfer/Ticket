3c4371b4355daf8f57a7259a691e628a0fdb7ac0
Audiosurfer <vassauder@gmail.com>
2025-08-20 09:19:28 +0200

    initial commit

---
+++ src/test/java/com/simple/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
